2ff6571d46ada1fffd26f157c973a35a17d33ac5
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-10-21 21:17:53 +0200

    Add rc files

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zlogin ~/.zlogin
+ln -s ~/.dotfiles/my-zsh/plugins/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/xinitrc ~/.xinitrc
+
+ln -s ~/.dotfiles/Xresources ~/.Xresources
+ln -s ~/.dotfiles/Xresources ~/.Xdefaults
