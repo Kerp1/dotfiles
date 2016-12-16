a2346866c86d54828dbd5c6a5b7a4fd0cf81be63
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-12-16 14:45:49 +0100

    Add lock.sh

---
+++ lock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://github.com/meskarune/i3lock-fancy/
+
+# Dependencies:
+# imagemagick
+# i3lock-color-git
+# scrot
+
+IMAGE=/tmp/i3lock.png
+BLURTYPE="4x6"
+
+scrot $IMAGE
+convert $IMAGE -level 0%,100%,0.6 -blur $BLURTYPE $IMAGE
+i3lock -u -i $IMAGE
