424021e5f8859faf2e62b7038397e2439edfb307
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-12-23 12:13:46 +0100

    Update install script to symlink configuration files

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#Directory where the dotfiles are located.
 DOTFILES_DIR=$HOME/.dotfiles
+#Directory where the configuration files should be symlinked to.
+CONFIG_DIR=$HOME/.config
+#List of directories that a symlink to a config file should be created in.
+CONFIG_DIRECTORIES=("i3" "i3status")
 
 ln -si $DOTFILES_DIR/oh-my-zsh $HOME/.oh-my-zsh
 ln -si $DOTFILES_DIR/zshrc $HOME/.zshrc
@@ -14,3 +19,12 @@ ln -si $DOTFILES_DIR/Xresources $HOME/.Xresources
 ln -si $DOTFILES_DIR/Xresources $HOME/.Xdefaults
 
 sh $DOTFILES_DIR/bin/urxvt-tabbed/install
+
+
+mkdir $CONFIG_DIR
+for directory in "${CONFIG_DIRECTORIES[@]}"
+do
+  echo "Symlinking configuration for $directory"
+  mkdir $CONFIG_DIR/$directory
+  ln -si $DOTFILES_DIR/config/$directory/config $CONFIG_DIR/$directory/config
+done
