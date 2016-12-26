7b633d784047cea575a6a2541f92dcd0d6ce3fb7
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-12-26 21:35:59 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,27 +4,25 @@
 DOTFILES_DIR=$HOME/.dotfiles
 #Directory where the configuration files should be symlinked to.
 CONFIG_DIR=$HOME/.config
-#List of directories that a symlink to a config file should be created in.
-CONFIG_DIRECTORIES=("i3" "i3status")
 
-ln -si $DOTFILES_DIR/oh-my-zsh $HOME/.oh-my-zsh
-ln -si $DOTFILES_DIR/zshrc $HOME/.zshrc
-ln -si $DOTFILES_DIR/zlogin $HOME/.zlogin
-ln -si $DOTFILES_DIR/my-zsh/plugins/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s $DOTFILES_DIR/oh-my-zsh $HOME/.oh-my-zsh
+ln -s $DOTFILES_DIR/zshrc $HOME/.zshrc
+ln -s $DOTFILES_DIR/zlogin $HOME/.zlogin
+ln -s $DOTFILES_DIR/my-zsh/plugins/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-ln -si $DOTFILES_DIR/vimrc $HOME/.vimrc
-ln -si $DOTFILES_DIR/xinitrc $HOME/.xinitrc
+ln -s $DOTFILES_DIR/vimrc $HOME/.vimrc
+ln -s $DOTFILES_DIR/xinitrc $HOME/.xinitrc
 
-ln -si $DOTFILES_DIR/Xresources $HOME/.Xresources
-ln -si $DOTFILES_DIR/Xresources $HOME/.Xdefaults
+ln -s $DOTFILES_DIR/Xresources $HOME/.Xresources
+ln -s $DOTFILES_DIR/Xresources $HOME/.Xdefaults
 
 sh $DOTFILES_DIR/bin/urxvt-tabbed/install
 
 
 mkdir $CONFIG_DIR
-for directory in "${CONFIG_DIRECTORIES[@]}"
+for directory in `ls config`
 do
   echo "Symlinking configuration for $directory"
   mkdir $CONFIG_DIR/$directory
-  ln -si $DOTFILES_DIR/config/$directory/config $CONFIG_DIR/$directory/config
+  ln -s $DOTFILES_DIR/config/$directory/config $CONFIG_DIR/$directory/config
 done
