599ccbf863547e84157faea38f9cf2867471b4d4
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2018-06-09 09:24:01 +0200

    Remove Arch specific files

--- config/polybar/spotify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $(playerctl status 2>/dev/null) = "Playing" ]]; then
-  title=$(exec playerctl metadata xesam:title)
-  artist=$(exec playerctl metadata xesam:artist)
-  echo " $title - $artist"
-else
-  echo ""
-fi
